64345d37cd297ebc28e751cc80e6f5387ed8258f
Aa Ezha <aaezha@gmail.com>
2021-12-17 14:21:42 +0700

    heroku-config

--- config/database.php
+++ config/database.php
@@ -2,11 +2,11 @@
 
 use Illuminate\Support\Str;
 
-// $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-// $host = $url["host"] ?? null;
-// $username = $url["user"] ?? null;
-// $password = $url["pass"] ?? null;
-// $database = substr($url["path"], 1);
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$host = $url["host"] ?? null;
+$username = $url["user"] ?? null;
+$password = $url["pass"] ?? null;
+$database = substr($url["path"], 1);
 
 return [
 
@@ -56,18 +56,18 @@ return [
             'url' => env('DATABASE_URL'),
 
             // heroku
-            // 'host' => $host,
-            // 'port' => env('DB_PORT', '3306'),
-            // 'database' => $database,
-            // 'username' => $username,
-            // 'password' => $password,
+            'host' => $host,
+            'port' => env('DB_PORT', '3306'),
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
 
             // localhost
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            // 'host' => env('DB_HOST', '127.0.0.1'),
+            // 'port' => env('DB_PORT', '3306'),
+            // 'database' => env('DB_DATABASE', 'forge'),
+            // 'username' => env('DB_USERNAME', 'forge'),
+            // 'password' => env('DB_PASSWORD', ''),
 
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
